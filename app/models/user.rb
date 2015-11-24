86e81f212c2e944b239b3d07a8d7a67e73f89ab2
sephb1118 <sephb1118@gmail.com>
2015-11-24 15:14:54 -0500

    Finishing user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :beers, dependent: :destroy
+  has_many :comments, through: :beers
   before_save { email.downcase! }
+  #attr_accessible :name, :email, :password, :password_confirmation
   validates :name, presence: true, length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255},
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'users/new'
-
+  get 'signup' => 'users#new'
+  resources :users
+  
   resources :beers do
     resources :comments
   end
